ca926b9b20c6ecc095d675052164bc5442f868dd
Abdurrahmans <abdurrahmansmark19972gmail.com>
2025-01-14 13:31:23 +0600

    database sqlmodel connection fastapi

---
+++ src/db/main.py
@@ -0,0 +1,20 @@
+from  sqlmodel import create_engine,text,SQLModel
+from sqlalchemy.ext.asyncio import AsyncEngine
+
+from src.config import settings
+
+
+engine = AsyncEngine(
+
+    create_engine(
+        url=settings.POSTGRES_URL,
+        echo=True
+        ))
+ 
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        from src.books.models import Book
+
+        await conn.run_sync(SQLModel.metadata.create_all)

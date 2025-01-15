8b2b021fc743ee6dc97ebdda643723469c8b1d2d
Abdurrahmans <abdurrahmansmark19972gmail.com>
2025-01-16 00:53:55 +0600

    Create access and refresh token

--- src/config.py
+++ src/config.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
     This class defines the settings for the app
     """
     POSTGRES_URL: str
+    JWT_SECRET:str
+    JWT_ALGORITHM:str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
